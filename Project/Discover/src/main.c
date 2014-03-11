9c27fb34d578620f1df84908cb69fbafa71f139f
wy193777 <421036289@qq.com>
2014-03-11 16:56:00 +0800

    The Official example could work!

--- Project/Discover/src/main.c
+++ Project/Discover/src/main.c
@@ -1,5 +1,4 @@
 
-/* Includes ------------------------------------------------------------------*/
 #include "stm8l15x.h"
 
 /** @addtogroup STM8L15x_StdPeriph_Examples
@@ -13,12 +12,12 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 #define TIM1_PERIOD             65535
-#define TIM1_PRESCALER              39999
+#define TIM1_PRESCALER              1
 #define TIM1_REPTETION_COUNTER      0
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-__IO uint16_t CCR1_Val = 30000;
+__IO uint16_t CCR1_Val = 32768;
 __IO uint16_t CCR2_Val = 16384;
 __IO uint16_t CCR3_Val = 8192;
 
@@ -50,7 +49,9 @@ void main(void)
 }
 
 /**
-  * @brief  Configure peripherals 
+  * @brief  Configure peripherals Clock   
+  * @param  None
+  * @retval None
   */
 static void CLK_Config(void)
 {
@@ -58,12 +59,15 @@ static void CLK_Config(void)
   CLK_PeripheralClockConfig(CLK_Peripheral_TIM1, ENABLE);
 }
 
+/**
+  * @brief  Configure TIM Channels GPIO 
+  * @param  None
+  * @retval None
+  */
 static void GPIO_Config(void)
 {
   /* GPIOD configuration: TIM1 channel 1 (PD2), channel 2 (PD4) and channel 3 (PD5) */
   GPIO_Init(GPIOD, GPIO_Pin_2 | GPIO_Pin_4 | GPIO_Pin_5, GPIO_Mode_Out_PP_Low_Fast);
-  GPIO_Init(GPIOE, GPIO_Pin_7, GPIO_Mode_Out_PP_High_Fast);
-  //GPIO_SetBits(GPIOE, GPIO_Pin_7);
 }
 
 /**
@@ -75,23 +79,21 @@ static void TIM1_Config(void)
 {
   /*
   - TIM1CLK = 2 MHz
-  - TIM1 counter clock = TIM1CLK / TIM1_PRESCALER+1 = 2 MHz/39999+1 = 50 Hz
+  - TIM1 counter clock = TIM1CLK / TIM1_PRESCALER+1 = 2 MHz/1+1 = 1 MHz
   */
   /* Time base configuration */
   TIM1_TimeBaseInit(TIM1_PRESCALER, TIM1_CounterMode_Up, TIM1_PERIOD, TIM1_REPTETION_COUNTER);
 
   /*
-  - The TIM1 CCR1 register value is equal to 3000: 
-  - So the TIM1 Channel 1 generates a periodic signal with a frequency equal to 50 Hz.
-  - PD2
+  - The TIM1 CCR1 register value is equal to 32768: 
+  - CC1 update rate = TIM1 counter clock / CCR1_Val = 30.51 Hz,
+  - So the TIM1 Channel 1 generates a periodic signal with a frequency equal to 15.25 Hz.
   */
-  /* PWM1 Mode configuration: Channel1 */
-  TIM1_OC1Init(TIM1_OCMode_PWM1, TIM1_OutputState_Enable, TIM1_OutputNState_Disable,
+  /* Toggle Mode configuration: Channel1 */
+  TIM1_OC1Init(TIM1_OCMode_Toggle, TIM1_OutputState_Enable, TIM1_OutputNState_Disable,
                CCR1_Val, TIM1_OCPolarity_Low, TIM1_OCNPolarity_Low, TIM1_OCIdleState_Set,
                TIM1_OCNIdleState_Set);
-  TIM1_CCxCmd(TIM1_Channel_1, ENABLE);
-  TIM1_SetCompare1(15000);
-  TIM1_OC1PreloadConfig(ENABLE);
+  TIM1_OC1PreloadConfig(DISABLE);
 
   /*
   - The TIM1 CCR2 register is equal to 16384:
@@ -131,6 +133,13 @@ static void TIM1_Config(void)
 
 #ifdef  USE_FULL_ASSERT
 
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t* file, uint32_t line)
 {
   /* User can add his own implementation to report the file name and line number,
